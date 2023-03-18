b3081a11aa0a6c76013f08e66c9105db14c5912f
阿辉 <oncwnuEXKikeT96xJEFPh9GH37UM@git.weixin.qq.com>
2023-03-18 21:09:50 +0800

    init

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,81 @@
+<script setup>
+const props = defineProps({
+  // Define props here
+  bgColor: {
+    type: String,
+    default: '#ffffff'
+  },
+  textColor: {
+    type: String,
+    default: '#000000'
+  },
+  borderColor: {
+    type: String,
+    default: '#000000'
+  },
+  hoverBgColor: {
+    type: String,
+    default: '#ffffff'
+  },
+  hoverTextColor: {
+    type: String,
+    default: '#000000'
+  },
+  hoverBorderColor: {
+    type: String,
+    default: '#000000'
+  },
+  focusBgColor: {
+    type: String,
+    default: '#ffffff'
+  },
+  focusTextColor: {
+    type: String,
+    default: '#000000'
+  },
+  focusBorderColor: {
+    type: String,
+    default: '#000000'
+  },
+})
+const _style = {
+  backgroundColor: props.bgColor,
+  color: props.textColor,
+  borderColor: props.borderColor,
+  hoverBgColor: props.hoverBgColor,
+  hoverTextColor: props.hoverTextColor,
+  hoverBorderColor: props.hoverBorderColor,
+  focusBgColor: props.focusBgColor,
+  focusTextColor: props.focusTextColor,
+  focusBorderColor: props.focusBorderColor,
+}
+</script>
+<template>
+  <div class="button-item h-full w-full flex flex-col items-center justify-center">
+    <button
+      class="shadow inline-flex items-center justify-center px-8 py-3 text-base font-medium leading-6 transition duration-150 ease-in-out bg-white border border-transparent rounded-md focus:outline-none custom-btn">
+      Button
+    </button>
+  </div>
+</template>
+
+<style lang="scss">
+.custom-btn {
+  background-color: bind('_style.bgColor');
+  color: bind('_style.textColor');
+  border-color: bind('_style.borderColor');
+
+  &:hover {
+    background-color: bind('_style.hoverBgColor');
+    color: bind('_style.hoverTextColor');
+    border-color: bind('_style.hoverBorderColor');
+  }
+
+  &:focus {
+    background-color: bind('_style.focusBgColor');
+    color: bind('_style.focusTextColor');
+    border-color: bind('_style.focusBorderColor');
+  }
+
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+import CustomButton from '@/components/CustomButton.vue';
+
+</script>
+
+<template>
+  <div class="bg-gray-50">
+    <h2 class="text-center mt-5 text-2xl">Let's custom my button!</h2>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 sm:px-0">
+        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
+          <div class="h-full grid grid-cols-3 grid-rows-1">
+            <CustomButton />
+            <div class="color-picker">
+              <input type="color" id="head" name="head" value="#e66465">
+              <input type="color" id="body" name="body" value="#f6f7eb">
+              <input type="color" id="legs" name="legs" value="#f6f7eb">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
